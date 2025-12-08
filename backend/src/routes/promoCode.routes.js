9d2ab5730f7c2dd7d664dd4065ec5f54d60c66d3
Zayed022 <140847182+Zayed022@users.noreply.github.com>
2025-12-08 14:10:02 +0530

    Fix minor bug

--- backend/src/routes/promoCode.routes.js
+++ backend/src/routes/promoCode.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { applyPromo, createPromoCode, deletePromoCode, getAllPromoCodes, updatePromoCode } from "../controllers/promoCode.controllers.js";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 
 const router = express.Router();
 
-router.post("/apply-promo", applyPromo);
+router.post("/apply-promo", verifyJWT,applyPromo);
 router.get("/", getAllPromoCodes);
 router.post("/", createPromoCode);
 router.put("/:id", updatePromoCode
